793f131fa9b4fa5fe0fd60ab92b0b65d2ad536fa
Kensuke Machida <ken@MacBook-Pro.local>
2020-05-10 15:16:03 +0900

    [add] title

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>じゃんけんゲーム</h2>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
